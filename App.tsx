89a009b4900a7f3caa7a3bb3ce32599b08199fa8
Sandeep Mathew <sandeepmathew098@gmail.com>
2024-11-28 20:46:59 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000000',
+  },
+});
+
+export default App;
